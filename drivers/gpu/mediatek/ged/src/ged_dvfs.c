00f68aef33ad56e659b2e655b6df997ce388b2cd
techyminati <sinha.aryan03@gmail.com>
2024-05-18 17:35:37 +0000

    drivers: mediatek: ged: Notify MediaTek gpueb of GPU frequency changes
    * On some newer SoCs, MediaTek started using a co-processor called "gpueb". It has its own firmware and is used to control GPU clocks over IPI.
    * The Mali driver needs to be aware of the frequency of the top-level GPU clock, but the GPU frequency changes are not always propagated correctly on AOSP due to missing timestamp data. MediaTek's BSP uses a BufferQueue in GED_KPI to send buffer information to kernel space for DVFS calculation. However, on AOSP, the Mali GPU driver is not aware of frequency changes due to missing frame timestamps, leading to DVFS instability.
    * This code adds a check to detect frequency changes and manually notifies the system of the new GPU frequency using mtk_notify_gpu_freq_change().
    * Note that only MediaTek devices with Mali GPU are affected by this bug. MediaTek PowerVR devices have no issues at all.
    
    Change-Id: I61cb2be1e04dfddbffb3a854214965af3cb139ec
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- drivers/gpu/mediatek/ged/src/ged_dvfs.c
+++ drivers/gpu/mediatek/ged/src/ged_dvfs.c
@@ -604,6 +604,14 @@ bool ged_dvfs_gpu_freq_commit(unsigned long ui32NewFreqID,
 			}
 		}
 
+#if defined(CONFIG_MTK_GPUFREQ_V2)
+		cur_freq = gpufreq_get_cur_freq(TARGET_GPU);
+		if (g_ged_gpueb_support && cur_freq != pre_freq) {
+			mtk_notify_gpu_freq_change(0, cur_freq);
+			pre_freq = cur_freq;
+		}
+#endif /* CONFIG_MTK_GPUFREQ_V2 */
+
 		if (ged_is_fdvfs_support()
 			&& is_fb_dvfs_triggered && is_fdvfs_enable()) {
 			memset(batch_freq, 0, sizeof(batch_freq));
